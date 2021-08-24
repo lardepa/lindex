efe5db62b50e5ee95540f815f86e73087fe2e410
Paul Girard <paul@ouestware.com>
2021-08-24 11:08:28 +0200

    Better import workflow: - use .env file - import tables in sequence - store and use last import date - update json file incrementally

--- backend/import_airtable.ts
+++ backend/import_airtable.ts
@@ -1,46 +1,74 @@
 const { AirtableBase } = require("airtable/lib/airtable_base");
-const fs = require("fs");
-const Airtable = require("airtable");
+import fs from "fs";
+import Airtable from "airtable";
+
+// load .env variables into process.env
+require("dotenv").config();
 
 const dumpObjects = (
   base: any,
   table: string,
   lastImportDate?: string | null
 ) => {
-  // retrieve new objects from airtable and save them on disk as a map by id
-  const selectOptions: any = {
-    maxRecords: 10,
-  };
-  if (lastImportDate) {
-    selectOptions.filterByFormula = `IS_AFTER(LAST_MODIFIED_TIME(), DATETIME_PARSE(${lastImportDate}, 'YYYY-MM-DD HH:mm'))`;
-  }
-  const data: { [key: string]: any } = {};
-  base(table)
-    .select(selectOptions)
-    .eachPage(
-      (records, fetchNextPage) => {
-        records.forEach((record) => {
-          // copy data in memory
-          data[record.id] = { ...record.fields };
-        });
-        console.log("end page");
-        fetchNextPage();
-      },
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
+  return new Promise((resolve, reject) => {
+    // retrieve new objects from airtable and save them on disk as a map by id
+    const selectOptions: any = {};
+    if (lastImportDate) {
+      selectOptions.filterByFormula = `IS_AFTER(LAST_MODIFIED_TIME(),"${lastImportDate}")`;
+    }
+    const data: { [key: string]: any } = {};
+    const page = 1;
+    base(table)
+      .select(selectOptions)
+      .eachPage(
+        (records, fetchNextPage) => {
+          console.log(`${table}: fetched ${records.length} from page ${page}`);
+          records.forEach((record) => {
+            // copy data in memory
+            data[record.id] = { ...record.fields };
+          });
+          fetchNextPage();
+        },
+        (err) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          }
+          console.log(`${table}: done`);
+          resolve(data);
         }
-        console.log("done");
-        // storing on disk
-        fs.writeFileSync(`data/${table}.json`, JSON.stringify(data, null, 2));
-        return data;
-      }
-    );
+      );
+  });
 };
 
-const base = Airtable.base("appVjnrpPJkvEZkDg");
+const importAllTables = async () => {
+  const base = Airtable.base(process.env.AIRTABLE_BASE);
+  // get last import date
+  let lastImportDate: string | null = null;
+  if (fs.existsSync(".last_import_date"))
+    lastImportDate = fs.readFileSync(".last_import_date").toString();
 
-for (let table of ["lieux", "professionnels", "périodes", "média"]) {
-  dumpObjects(base, table);
-}
+  try {
+    for (let table of ["lieux", "professionnels", "périodes", "média"]) {
+      const data: { [key: string]: any } = await dumpObjects(
+        base,
+        table,
+        lastImportDate
+      );
+      // update existingData
+
+      const existingData = fs.existsSync(`data/${table}.json`)
+        ? JSON.parse(fs.readFileSync(`data/${table}.json`).toString())
+        : {};
+      const newData = { ...existingData, ...data };
+      // storing on disk
+      fs.writeFileSync(`data/${table}.json`, JSON.stringify(newData, null, 2));
+    }
+    // write import date into disk
+    fs.writeFileSync(`.last_import_date`, new Date().toISOString());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+importAllTables();
