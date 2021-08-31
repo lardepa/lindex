4b2638c009d0d5edf9fd1a232d2763131e09c7ef
Paul Girard <paul@ouestware.com>
2021-08-31 14:32:56 +0200

    [frontend] home style

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -17,22 +17,24 @@ export const Home: React.FC<{}> = () => {
   }, []);
 
   return (
-    <div className="container-xl" style={{ position: "relative" }}>
+    <div className="container-fluid" style={{ position: "relative" }}>
       <Logo />
       <div className="row full-height no-gutters">
-        <div className="col-sm  d-flex flex-column justify-content-center px-0">
-          <div className="presentation">
-            Texte de présentation de la cartographie de l'Ardepa... azebzoae hzaomi jezmoejzami zjmazioj mzaioej zamijza
-            miozaje mzoaij ezmaoijzmoijza emoiza jmzioj
-          </div>
-          <div className="menu">
-            <div>Explorer la carte</div>
-            <div>Sélections des invités</div>
-            <div>Parcours</div>
-            <div>Random</div>
+        <div className="col-sm-6 col-xl-4 px-0 map-full-height overflow-scroll d-flex flex-column justify-content-start">
+          <div className="d-flex flex-column" style={{ margin: "auto 0" }}>
+            <div className="presentation">
+              Texte de présentation de la cartographie de l'Ardepa... azebzoae hzaomi jezmoejzami zjmazioj mzaioej
+              zamijza miozaje mzoaij ezmaoijzmoijza emoiza jmzioj
+            </div>
+            <div className="menu">
+              <div>Explorer la carte</div>
+              <div>Sélections des invités</div>
+              <div>Parcours</div>
+              <div>Random</div>
+            </div>
           </div>
         </div>
-        <div className="col-sm px-0">
+        <div className="col-sm-6 col-xl-8 px-0">
           <Map lieux={lieux} className="map-full-height" />
         </div>
       </div>
