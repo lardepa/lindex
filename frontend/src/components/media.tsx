e93656a24e1009de8f53b70a8dbe3cf802e4c92a
Paul Girard <paul@ouestware.com>
2021-09-08 16:03:06 +0200

    use cloud host url for media

--- frontend/src/components/media.tsx
+++ frontend/src/components/media.tsx
@@ -6,6 +6,8 @@ import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack";
 // issue with webworker load through webpack see https://github.com/wojtekmaj/react-pdf/issues/291
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+//TODO: move to separate file?
+//TODO: add next/pevious page buttons
 const PDF: React.FC<{ file: string }> = ({ file }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber] = useState<number>(1);
@@ -34,16 +36,15 @@ export const Media: React.FC<{ media: MediaType }> = ({ media }) => {
         {" "}
         {media.fichiers.map((f) => {
           const ext = f.type.split("/").slice(-1)[0];
-          console.log(`${config.DATA_URL}/attachments/${f.id}/full.${ext}`);
           switch (ext) {
             case "pdf":
               // PDF file is stored under full quality (other are thumbnail of first page)
-              return <PDF key={f.id} file={`${config.DATA_URL}/attachments/${f.id}/full.${ext}`} />;
+              return <PDF key={f.id} file={f.url} />; // {`${config.DATA_URL}/attachments/${f.id}/full.${ext}`} />;
             default:
               return (
                 <img
                   key={f.id}
-                  src={`${config.DATA_URL}/attachments/${f.id}/large.${ext}`}
+                  src={f.thumbnails?.large.url} //{`${config.DATA_URL}/attachments/${f.id}/large.${ext}`}
                   alt={media.nom}
                   title={media.credits}
                 />
