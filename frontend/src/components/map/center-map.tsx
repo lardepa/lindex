4c70170afee2fc224b55e58a768b448a2d10909d
Paul Girard <paul@ouestware.com>
2021-09-13 23:28:52 +0200

    [frontend] map fitBounds lifecycle

---
+++ frontend/src/components/map/center-map.tsx
@@ -0,0 +1,38 @@
+import { LatLngBoundsLiteral, LatLngExpression } from "leaflet";
+import { max, min } from "lodash";
+import { useEffect } from "react";
+import { useMap } from "react-leaflet";
+import { LieuType } from "../../types";
+
+export const CenterMap = (props: { lieux: LieuType[] }) => {
+  const map = useMap();
+  const { lieux } = props;
+  useEffect(() => {
+    // calculate bounds of lieux geo points
+    const { lats, lngs } = lieux.reduce(
+      (all: { lats: number[]; lngs: number[] }, lieu: LieuType) => {
+        if (lieu.geolocalisation)
+          return { lats: [...all.lats, lieu.geolocalisation[0]], lngs: [...all.lngs, lieu.geolocalisation[1]] };
+        else return all;
+      },
+      { lats: [], lngs: [] },
+    );
+    let bounds: [number, number][] | undefined = undefined;
+    let center: [number, number] | undefined = undefined;
+    let zoom: number | undefined = undefined;
+    if (lats && lats.length > 0 && lngs && lngs.length > 0) {
+      // if we have geolocalized points, fit to those
+      bounds = [];
+      bounds.push([min(lats) as number, max(lngs) as number]);
+      bounds.push([max(lats) as number, min(lngs) as number]);
+    } else {
+      // otherwise center on Nantes
+      center = [47.207562, -1.557635];
+      zoom = 15;
+    }
+
+    if (bounds) map.fitBounds(bounds as LatLngBoundsLiteral);
+    else map.setView(center as LatLngExpression, zoom);
+  }, [lieux, map]);
+  return null;
+};
--- frontend/src/components/map/map.tsx
+++ frontend/src/components/map/map.tsx
@@ -9,8 +9,8 @@ import { LatLngExpression } from "leaflet";
 
 import { Media } from "../media";
 import { Link } from "react-router-dom";
-import { max, min } from "lodash";
 import { MarkerIcon } from "./marker-icon";
+import { CenterMap } from "./center-map";
 
 interface MapProps {
   lieux: LieuType[];
@@ -23,31 +23,9 @@ export const Map: React.FC<MapProps> = (props) => {
 
   // TODO: Change marker pixels size according to zoom? https://jsfiddle.net/falkedesign/nobapxvd/
 
-  // calculate bounds of lieux geo points
-  const { lats, lngs } = lieux.reduce(
-    (all: { lats: number[]; lngs: number[] }, lieu: LieuType) => {
-      if (lieu.geolocalisation)
-        return { lats: [...all.lats, lieu.geolocalisation[0]], lngs: [...all.lngs, lieu.geolocalisation[1]] };
-      else return all;
-    },
-    { lats: [], lngs: [] },
-  );
-  let bounds: [number, number][] | undefined = undefined;
-  let center: [number, number] | undefined = undefined;
-  let zoom: number | undefined = undefined;
-  if (lats && lats.length > 0 && lngs && lngs.length > 0) {
-    // if we have geolocalized points, fit to those
-    bounds = [];
-    bounds.push([min(lats) as number, max(lngs) as number]);
-    bounds.push([max(lats) as number, min(lngs) as number]);
-  } else {
-    // otherwise center on Nantes
-    center = [47.207562, -1.557635];
-    zoom = 15;
-  }
-
   return (
-    <MapContainer bounds={bounds} center={center} zoom={zoom} scrollWheelZoom={true} className={className}>
+    <MapContainer center={[47.207562, -1.557635]} zoom={15} scrollWheelZoom={true} className={className}>
+      <CenterMap lieux={lieux} />
       <TileLayer
         attribution={config.MAP_LAYERS[config.MAP_LAYER].TILE_CREDITS}
         url={config.MAP_LAYERS[config.MAP_LAYER].TILE_URL}
