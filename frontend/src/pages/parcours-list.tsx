c6f109e2f217a2915655ee1242866bf6636d6751
Paul Girard <paul@ouestware.com>
2021-11-12 14:23:19 +0100

    [client] parcourslist and selectionlist responsive relates #25

--- frontend/src/pages/parcours-list.tsx
+++ frontend/src/pages/parcours-list.tsx
@@ -6,46 +6,57 @@ import { PageLayout } from "../components/layout/page-layout";
 import { LinkPreview } from "../components/link-preview";
 import { useGetList } from "../hooks/useAPI";
 import { Loader } from "../components/loader";
+import withSize from "../components/layout/with-size";
+import config from "../config";
 
-export const ParcoursListPage: React.FC<{}> = () => {
+const _ParcoursListPage: React.FC<{ width: number }> = ({ width }) => {
   const [parcours, loading] = useGetList<ParcoursType>("parcours");
+  const smallScreen = width && width <= config.RESPONSIVE_BREAKPOINTS.sm;
+
+  const parcoursListWithTitle = (
+    <>
+      <div className="presentation">Retrouvez les parcours proposés par l'Ardepa.</div>
+      <div className="parcours-list">
+        {parcours?.map((p) => (
+          <LinkPreview to={`/parcours/${p.id}`}>
+            <div key={p.id} className="parcours-card">
+              {p.cover_media && p.cover_media?.fichiers && (
+                <div
+                  className="parcours-cover"
+                  style={{
+                    backgroundImage: `url(${p.cover_media?.fichiers[0].thumbnails?.large?.url})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center",
+                  }}
+                ></div>
+              )}
+              <div className="parcours-title">
+                <div className="title">{p.nom}</div>
+                <div className="subtitle">{p["sous-titre"]}</div>
+              </div>
+            </div>
+          </LinkPreview>
+        ))}
+      </div>
+    </>
+  );
 
   return (
     <PageLayout
       menuSelectedItem="parcours"
-      leftContent={
-        <>
-          <div className="presentation">Retrouvez les parcours proposés par l'Ardepa.</div>
-          <div className="parcours-list">
-            {parcours?.map((p) => (
-              <LinkPreview to={`/parcours/${p.id}`}>
-                <div key={p.id} className="parcours-card">
-                  {p.cover_media && p.cover_media?.fichiers && (
-                    <div
-                      className="parcours-cover"
-                      style={{
-                        backgroundImage: `url(${p.cover_media?.fichiers[0].thumbnails?.large?.url})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center center",
-                      }}
-                    ></div>
-                  )}
-                  <div className="parcours-title">
-                    <div className="title">{p.nom}</div>
-                    <div className="subtitle">{p["sous-titre"]}</div>
-                  </div>
-                </div>
-              </LinkPreview>
-            ))}
-          </div>
-        </>
-      }
+      leftContent={smallScreen ? null : parcoursListWithTitle}
       rightContent={
-        <div style={{ gridArea: "2/1/2/4" }}>
-          {!loading && parcours && <Map lieux={flatten(parcours.map((p) => p.lieux))} className="explore-map" />}
-          <Loader loading={loading} />
-        </div>
+        smallScreen ? (
+          <div style={{ gridArea: "col-content", marginTop: "2rem" }}>{parcoursListWithTitle}</div>
+        ) : (
+          <div style={{ gridArea: "2/1/2/4" }}>
+            {!loading && parcours && <Map lieux={flatten(parcours.map((p) => p.lieux))} className="explore-map" />}
+            <Loader loading={loading} />
+          </div>
+        )
       }
     />
   );
 };
+
+export const ParcoursListPage = withSize<{}>(_ParcoursListPage);
--- frontend/src/pages/selections-list.tsx
+++ frontend/src/pages/selections-list.tsx
@@ -6,50 +6,55 @@ import { PageLayout } from "../components/layout/page-layout";
 import { LinkPreview } from "../components/link-preview";
 import { useGetList } from "../hooks/useAPI";
 import { Loader } from "../components/loader";
+import withSize from "../components/layout/with-size";
+import config from "../config";
 
-export const SelectionsListPage: React.FC<{}> = () => {
+const _SelectionsListPage: React.FC<{ width: number }> = ({ width }) => {
   const [selections, loading] = useGetList<SelectionType>("selections");
+  const smallScreen = width && width <= config.RESPONSIVE_BREAKPOINTS.sm;
 
+  const selectionsListWithTitle = (
+    <>
+      <div className="presentation">L'Ardepa invite des citoyens à vous faire découvrir l'architecture.</div>
+      <div className="parcours-list">
+        {selections?.map((s) => (
+          <LinkPreview to={`/selections/${s.id}`}>
+            <div key={s.id} className="parcours-card">
+              {s.portrait && s.portrait?.fichiers && (
+                <div
+                  className="parcours-cover"
+                  style={{
+                    backgroundImage: `url(${s.portrait?.fichiers[0].thumbnails?.large?.url})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center",
+                  }}
+                ></div>
+              )}
+              <div className="parcours-title">
+                <div className="title">{s.invité}</div>
+              </div>
+            </div>
+          </LinkPreview>
+        ))}
+      </div>{" "}
+    </>
+  );
   return (
     <PageLayout
       menuSelectedItem="selections"
-      leftContent={
-        <>
-          <div className="presentation">L'Ardepa invite des citoyens à vous faire découvrir l'architecture.</div>
-          <div className="parcours-list">
-            {selections?.map((s) => (
-              <LinkPreview to={`/selections/${s.id}`}>
-                <div key={s.id} className="parcours-card">
-                  {/* {s.portrait && (
-                    <div className="parcours-cover">
-                      <Media media={s.portrait} />
-                    </div>
-                  )} */}
-                  {s.portrait && s.portrait?.fichiers && (
-                    <div
-                      className="parcours-cover"
-                      style={{
-                        backgroundImage: `url(${s.portrait?.fichiers[0].thumbnails?.large?.url})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center center",
-                      }}
-                    ></div>
-                  )}
-                  <div className="parcours-title">
-                    <div className="title">{s.invité}</div>
-                  </div>
-                </div>
-              </LinkPreview>
-            ))}
-          </div>
-        </>
-      }
+      leftContent={smallScreen ? null : selectionsListWithTitle}
       rightContent={
-        <div style={{ gridArea: "2/1/2/4" }}>
-          {!loading && selections && <Map lieux={flatten(selections.map((s) => s.lieux))} className="explore-map" />}
-          <Loader loading={loading} />
-        </div>
+        smallScreen ? (
+          <div style={{ gridArea: "col-content", marginTop: "2rem" }}>{selectionsListWithTitle}</div>
+        ) : (
+          <div style={{ gridArea: "2/1/2/4" }}>
+            {!loading && selections && <Map lieux={flatten(selections.map((s) => s.lieux))} className="explore-map" />}
+            <Loader loading={loading} />
+          </div>
+        )
       }
     />
   );
 };
+
+export const SelectionsListPage = withSize<{}>(_SelectionsListPage);
