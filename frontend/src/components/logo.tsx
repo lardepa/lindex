84ff991133d8de208f588605ae9ef8f1497d1780
Paul Girard <paul@ouestware.com>
2021-11-22 14:15:17 +0100

    [Client] logo size

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import LogoSVG from "../assets/logo.svg";
+import LogoPNG from "../assets/logo_lindex.png";
 import { LinkPreview } from "./link-preview";
 
 export const Logo: React.FC<{}> = () => (
   <LinkPreview to="/" className="logo">
-    <img src={LogoSVG} alt="L'ardepa Carto" />
+    <img src={LogoPNG} alt="L'ardepa Carto" />
   </LinkPreview>
 );
