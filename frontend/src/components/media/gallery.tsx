875c6e3696885598a76b8ce47470d1819c4f78a4
Paul Girard <paul@ouestware.com>
2024-02-13 16:43:09 +0100

    [frontend] fullscreen image responsive issues

--- frontend/src/components/media/gallery.tsx
+++ frontend/src/components/media/gallery.tsx
@@ -5,6 +5,23 @@ import { MdClose, MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 import { MediaType } from "../../types";
 import { Media } from "./media";
 
+const getFullScreenMEdiaSizes = (media: MediaType): string | undefined => {
+  const file = media.fichiers && media.fichiers[0];
+  if (file && file && file.width && file.height) {
+    const orientation = file.width > file.height ? "landscape" : "portrait";
+
+    // fullscreen with landscape image => use 100vw until the screen is wider than the image itself in which case we don't oversize the image
+    // max-height does not work when setting width through sizes thus we add a media query on aspect ratio to cover this case.
+    if (orientation === "landscape")
+      return `(max-width: ${file.width}px) and (max-aspect-ratio: ${file.height / file.width}px) 100vw,(max-width: ${file.width}px) calc(100vh * ${file.width / file.height}),${file.width}px`;
+    else {
+      // fullscreen with portrait image => we adapt width to the screen height until the screen is higher than the image itself in which case we don't oversize the image
+      return `(max-height: ${file.height}px) calc(100vh * ${file.width / file.height}), ${file.width}px`;
+    }
+  }
+  return undefined;
+};
+
 export const MediaGallery: FC<{ medias: MediaType[]; sizes?: string }> = ({ medias, sizes }) => {
   const [fullscreenMediaIndex, setFullscreenMediaIndex] = useState<number | null>(null);
   const gallery = useRef<HTMLDivElement | null>(null);
@@ -19,9 +36,10 @@ export const MediaGallery: FC<{ medias: MediaType[]; sizes?: string }> = ({ medi
     medias.map((m) => (m.fichiers ? m.fichiers.map((f) => ({ ...m, fichiers: [f] })) : m)),
   ).filter(identity) as MediaType[];
 
+  const fullscreenMedia = fullscreenMediaIndex !== null ? flattenMedias[fullscreenMediaIndex] : null;
   return (
     <>
-      {fullscreenMediaIndex !== null ? (
+      {fullscreenMediaIndex !== null && fullscreenMedia ? (
         <div
           ref={gallery}
           className="media-full-screen-gallery"
@@ -36,7 +54,11 @@ export const MediaGallery: FC<{ medias: MediaType[]; sizes?: string }> = ({ medi
           }}
         >
           <div className="w-100 h-100 position-relative d-flex justify-content-center align-items-center">
-            <Media key={fullscreenMediaIndex} media={flattenMedias[fullscreenMediaIndex]} sizes="100vw" />
+            <Media
+              key={fullscreenMediaIndex}
+              media={fullscreenMedia}
+              sizes={getFullScreenMEdiaSizes(fullscreenMedia)}
+            />
           </div>
           <button
             type="button"
